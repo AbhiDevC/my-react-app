54eca8b0b8902aa4492b049f5b4399c7b57eadeb
Abhinandan Sharma <abhinandansharma@Abhinandans-MacBook-Pro.local>
2025-04-14 14:55:47 -0400

    added files to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const Pizza = (props) => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, props.name),
+      React.createElement("p", {}, props.description),
+    ]);
+  };
+  
+  const App = () => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Padre Gino's"),
+      React.createElement(Pizza, {
+        name: "The Pepperoni Pizza",
+        description: "Mozzarella Cheese, Pepperoni",
+      }),
+      React.createElement(Pizza, {
+        name: "The Hawaiian Pizza",
+        description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+      }),
+      React.createElement(Pizza, {
+        name: "The Big Meat Pizza",
+        description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+      }),
+    ]);
+  };
+  
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(App));
